c74f1cd713437fd64f25f1b528c2daa456178ac7
kakeru-ikeda <kakerumail1451@gmail.com>
2025-05-05 20:53:15 +0900

    feat: ESLint設定を更新し、TypeScriptおよびReact用のプラグインを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,79 @@
 module.exports = {
+    root: true,
     env: {
         browser: true,
-        es2021: true
+        es2021: true,
+        node: true,
     },
     extends: [
         'eslint:recommended',
-        'plugin:react/recommended'
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'next/core-web-vitals',
+        'prettier',
     ],
+    parser: '@typescript-eslint/parser',
     parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
         ecmaVersion: 12,
-        sourceType: 'module'
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint',
+        'import',
+    ],
+    settings: {
+        react: {
+            version: 'detect',
+        },
     },
     rules: {
-        'no-unused-vars': 'warn',
-        'no-console': 'off'
-    }
+        // TypeScript関連
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-unused-vars': ['warn', {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_'
+        }],
+
+        // React関連
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        'react/display-name': 'off',
+
+        // 一般
+        'no-console': ['warn', { allow: ['warn', 'error'] }],
+        'no-debugger': 'warn',
+        'prefer-const': 'warn',
+
+        // インポート順序
+        'import/order': [
+            'warn',
+            {
+                groups: [
+                    'builtin',
+                    'external',
+                    'internal',
+                    'parent',
+                    'sibling',
+                    'index',
+                    'object',
+                    'type',
+                ],
+                'newlines-between': 'always',
+                alphabetize: { order: 'asc', caseInsensitive: true },
+            },
+        ],
+    },
+    ignorePatterns: [
+        'node_modules/',
+        '.next/',
+        'out/',
+        'public/',
+        '**/*.d.ts',
+    ],
 };
